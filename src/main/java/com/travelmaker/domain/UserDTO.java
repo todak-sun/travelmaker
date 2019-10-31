f6a3fce0771242dfce60b8785da65fc495877070
todak_sun <tjsdydwn@gmail.com>
2019-10-31 12:54:24 +0900

    첫번째 커밋
    # travelmaker 프로젝트 생성
    # Spring 버전 5.2.0 사용

---
+++ src/main/java/com/travelmaker/domain/UserDTO.java
@@ -0,0 +1,28 @@
+package com.travelmaker.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+	private String nickname;
+	private String realname;
+	private String email;
+	private String email2;
+	private String password;
+	private Grade grade;
+	private Gender gender;
+	private String birthdate;
+	private String postcode;
+	private String addr1;
+	private String addr2;
+	private String phone;
+	private int agreeNeed;
+	private int agreeOption;
+	private String dateRegist;
+	private String imgProfile;
+	private String contentProfile;
+	private int warn;
+	private String account;
+}
