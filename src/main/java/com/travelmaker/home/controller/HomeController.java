11c2fe58cb5c84f4d146d963d95be870504f1ee7
todak_sun <tjsdydwn@gmail.com>
2019-10-31 17:20:17 +0900

    임시로 루트 매핑할 homecontroller 생성

---
+++ src/main/java/com/travelmaker/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.travelmaker.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		return "/main/main";
+	}
+}
