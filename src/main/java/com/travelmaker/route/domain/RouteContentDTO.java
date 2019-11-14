1d68169278cc9ed612893317ff4fbd99bb2e5e47
todak_sun <tjsdydwn@gmail.com>
2019-11-14 11:56:03 +0900

    git에 불필요한 파일 삭제

--- src/main/java/com/travelmaker/route/domain/RouteContentDTO.java
+++ src/main/java/com/travelmaker/route/domain/RouteContentDTO.java
@@ -1,12 +1,5 @@
 package com.travelmaker.route.domain;
 
-<<<<<<< HEAD
-=======
-import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
-
->>>>>>> 586671ae114d8b2a9c3d6a1660afc4ea93908b98
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,8 +21,4 @@ public class RouteContentDTO {
 	private int fixed;
 	private MultipartFile[] images;
 	private List<String> imgs;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 586671ae114d8b2a9c3d6a1660afc4ea93908b98
